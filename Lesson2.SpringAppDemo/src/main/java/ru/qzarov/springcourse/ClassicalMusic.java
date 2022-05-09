b2aa37a38480d5807b65743c8535f4cd88b6dfa6
Yaroslav Paroshin <qzarov@mail.ru>
2022-05-09 17:59:42 +0300

    12_Annotations. @Scope, @Value

--- Lesson2.SpringAppDemo/src/main/java/ru/qzarov/springcourse/ClassicalMusic.java
+++ Lesson2.SpringAppDemo/src/main/java/ru/qzarov/springcourse/ClassicalMusic.java
@@ -1,11 +1,13 @@
 package ru.qzarov.springcourse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
  * @author Qzarov
  */
 @Component
+@Scope("singleton")
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
--- Lesson2.SpringAppDemo/src/main/java/ru/qzarov/springcourse/TestSpring.java
+++ Lesson2.SpringAppDemo/src/main/java/ru/qzarov/springcourse/TestSpring.java
@@ -19,9 +19,15 @@ public class TestSpring {
         //MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         //musicPlayer.playMusic();
 
-        Computer computer = context.getBean("computer", Computer.class);
+        //Computer computer = context.getBean("computer", Computer.class);
+        //System.out.println(computer.toString());
 
-        System.out.println(computer.toString());
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        //ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
 
         context.close();
     }
