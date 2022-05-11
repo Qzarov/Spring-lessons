aa90ae0c3ad97149f488d39d30914860edff02ed
Yaroslav Paroshin <qzarov@mail.ru>
2022-05-11 14:11:04 +0300

    12_2_Annotations. @PostConstruct, @PreDestroy

--- Lesson2.SpringAppDemo/src/main/java/ru/qzarov/springcourse/ClassicalMusic.java
+++ Lesson2.SpringAppDemo/src/main/java/ru/qzarov/springcourse/ClassicalMusic.java
@@ -1,14 +1,29 @@
 package ru.qzarov.springcourse;
 
-import org.springframework.context.annotation.Scope;
+//import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 
 /**
  * @author Qzarov
  */
 @Component
-@Scope("singleton")
+//@Scope("prototype")
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doInit() {
+        System.out.println("Initialization: ClassicalMusic");
+    } 
+
+    @PreDestroy
+    public void doDestroy() {
+        System.out.println("Destruction: ClassicalMusic");
+    } 
+
     @Override
     public String getSong() {
         return "Tchaikovskiy - Swan's Lake";
--- Lesson2.SpringAppDemo/src/main/java/ru/qzarov/springcourse/TestSpring.java
+++ Lesson2.SpringAppDemo/src/main/java/ru/qzarov/springcourse/TestSpring.java
@@ -11,23 +11,13 @@ public class TestSpring {
             "applicationContext.xml"
         );
 
-        // Music musicBean = context.getBean("rockMusic", Music.class);
-
-        // MusicPlayer musicPlayer = new MusicPlayer(musicBean);
-        // musicPlayer.playMusic();
-
-        //MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        //musicPlayer.playMusic();
-
-        //Computer computer = context.getBean("computer", Computer.class);
-        //System.out.println(computer.toString());
-
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
 
-        //ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
+        classicalMusic.getSong();
 
         context.close();
     }
